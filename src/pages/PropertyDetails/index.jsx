718b493b0f401fdb9ad49d7ed4c4302bea51492c
afrguilherme <augustogafr@gmail.com>
2024-11-13 21:58:36 -0300

    PropertyDetails development

--- src/pages/PropertyDetails/index.jsx
+++ src/pages/PropertyDetails/index.jsx
@@ -1,8 +1,24 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
-import { Container, CarouselSection, CarouselStyles } from "./styles"
 import api from "../../services/api"
+
+import { formatDate, formatTime } from "../../utils/dateTime"
+import { formatCurrency } from "../../utils/formatUtils"
+
+import DefaultButton from "../../components/DefaultButton"
+
+import {
+  Container,
+  LeftSection,
+  Title,
+  PublishInfo,
+  CarouselSection,
+  CarouselStyles,
+  Price,
+  Description,
+  RightSection,
+} from "./styles"
 
 const PropertyDetails = () => {
   const { id } = useParams()
@@ -18,15 +34,28 @@ const PropertyDetails = () => {
 
   return (
     <Container>
-      <CarouselSection>
-        {property && (
-          <>
-            <CarouselStyles pagination={false} itemsToShow={1}>
-              {property && property.url.map((img) => <img src={img} />)}
-            </CarouselStyles>
-          </>
-        )}
-      </CarouselSection>
+      <LeftSection>
+        <Title>{property ? `${property.name}` : ""}</Title>
+        <PublishInfo>
+          {property
+            ? `Publicado em ${formatDate(property.createdAt)} às ${formatTime(
+                property.createdAt
+              )} - id. ${property.id}`
+            : ""}
+        </PublishInfo>
+        <CarouselSection>
+          {property && (
+            <>
+              <CarouselStyles pagination={false} itemsToShow={1}>
+                {property && property.url.map((img) => <img src={img} />)}
+              </CarouselStyles>
+            </>
+          )}
+        </CarouselSection>
+        <Price>{property ? `${formatCurrency(property.price)}` : ""}</Price>
+        <Description>{property ? `${property.description}` : ""}</Description>
+      </LeftSection>
+      <RightSection></RightSection>
     </Container>
   )
 }
--- src/pages/PropertyDetails/styles.js
+++ src/pages/PropertyDetails/styles.js
@@ -5,16 +5,35 @@ export const Container = styled.div`
   height: 100vh;
   width: 100%;
   display: flex;
-  padding: 50px 50px;
-
-  h1 {
-    color: #3c4453;
-  }
+  justify-content: space-between;
+  padding: 20px 50px;
 `
-export const CarouselSection = styled.div`
+export const LeftSection = styled.div`
   width: 50%;
-  height: 500px;
-  background-color: rgba(60, 68, 83, 0.4);
+`
+
+export const Title = styled.h1`
+  color: #3c4453;
+  font-size: 2em;
+  margin-bottom: 20px;
+`
+
+export const PublishInfo = styled.p`
+  color: #3c4453;
+  font-size: 16px;
+  font-weight: 500;
+
+  margin-bottom: 10px;
+`
+
+export const CarouselSection = styled.div`
+  width: 100%;
+  min-width: 40%;
+  height: max-content;
+  background-color: #ffffff;
+
+  border: 1px solid rgba(60, 68, 83, 0.4);
+  border-radius: 15px;
 `
 
 export const CarouselStyles = styled(Carousel)`
@@ -25,4 +44,26 @@ export const CarouselStyles = styled(Carousel)`
     width: 100%;
     max-width: 500px;
   }
+`
+
+export const Price = styled.h3`
+  color: #3c4453;
+  font-size: 28px;
+
+  margin-top: 20px;
+`
+
+export const Description = styled.p`
+  width: 100%;
+  color: #3c4453;
+  font-size: 16px;
+  font-weight: 500;
+
+  margin-top: 20px;
+`
+
+export const RightSection = styled.div`
+  width: 45%;
+
+  background-color: grey;
 `
